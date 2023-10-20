0ca3c294f73184ad421bd55571244f2dcfa4309c
idenshah <iden.shahsavar@gmail.com>
2023-10-19 22:17:31 -0400

    updated

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -11,7 +11,7 @@ const AboutPage = () => {
         </p>
         <div className="downloadCV">
           <a
-            href="https://drive.usercontent.google.com/download?id=1X-LaUneTNd_BS8OubN6xP8qs0-kmlj4j&export=download&authuser=0&confirm=t&uuid=f31a33a0-8d1a-439e-8d84-f22fa609cee2&at=APZUnTUMNb-kOeZK0amURDj6pIQ6:1696608361249"
+            href="https://drive.google.com/uc?export=download&id=1O3LJgRHQJySzM4zNGHV4fCAxuk3t6CyI"
             download
           >
             <img src="/image/downloadCV.png" alt="CV download link" />
